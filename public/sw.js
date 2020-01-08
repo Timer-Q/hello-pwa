7e692e5e60e5b3c4538c14ed7f28aded720ba309
qiguixu <qiguixu@xiaomi.com>
2020-01-08 20:32:44 +0800

    change

--- public/sw.js
+++ public/sw.js
@@ -7,9 +7,9 @@ self.addEventListener('install', async event => {
   // 在服务工作线程中，延长事件的寿命从而阻止浏览器在事件中的异步操作完成之前终止服务工作线程。
 
   // event.waitUntil(
-  //   // Cache 接口为缓存的 Request / Response  对象对提供存储机制，例如，作为ServiceWorker 生命周期的一部分。
-  //   // 请注意，Cache 接口像 workers 一样，是暴露在 window 作用域下的。
-  //   // 尽管它被定义在 service worker 的标准中,  但是它不必一定要配合 service worker 使用.
+  //  Cache 接口为缓存的 Request / Response  对象对提供存储机制，例如，作为ServiceWorker 生命周期的一部分。
+  //  请注意，Cache 接口像 workers 一样，是暴露在 window 作用域下的。
+  //  尽管它被定义在 service worker 的标准中,  但是它不必一定要配合 service worker 使用.
   //   caches.open('v1').then(function(cache) {
   //     return cache.addAll(['/', '/index.html', '/logo192.png', '/logo512.png', '/manifest.json']);
   //   }),
