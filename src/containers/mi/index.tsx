0d2ef5b407f45165754a386e75c1ab8f75603482
qiguixu <qiguixu@xiaomi.com>
2020-03-06 19:16:21 +0800

    init mi

---
+++ src/containers/mi/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Mi() {
+  return (
+    <div>
+      mi
+    </div>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Home from '../containers/home';
 import Detail from '../containers/detail';
+import Mi from '../containers/mi';
 
 export default function Routes() {
   return (
@@ -9,7 +10,8 @@ export default function Routes() {
       <Switch>
         <Route path='/detail' component={Detail} />
         <Route path='/home' component={Home} />
-        <Route path='/' render={() => <Redirect to='/home' />} />
+        <Route path='/mi' component={Mi} />
+        <Route path='/' render={() => <Redirect to='/mi' />} />
       </Switch>
     </Router>
   );
