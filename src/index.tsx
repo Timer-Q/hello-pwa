4bd490a5a7f040931bc5a4ab9a7e13e4a9d0fe7a
qiguixu <qiguixu@xiaomi.com>
2020-03-07 19:25:32 +0800

    暂停使用 pwa

--- src/index.tsx
+++ src/index.tsx
@@ -11,41 +11,41 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-    .register('./sw.js')
-    .then(registration => {
-      registration.onupdatefound = () => {
-        console.log("Service Worker 注册成功");
-        const installingWorker = registration.installing;
-        if (installingWorker == null) {
-          return;
-        }
-        installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed') {
-            if (navigator.serviceWorker.controller) {
-              // At this point, the updated precached content has been fetched,
-              // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
-              console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
-              );
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker
+//     .register('./sw.js')
+//     .then(registration => {
+//       registration.onupdatefound = () => {
+//         console.log("Service Worker 注册成功");
+//         const installingWorker = registration.installing;
+//         if (installingWorker == null) {
+//           return;
+//         }
+//         installingWorker.onstatechange = () => {
+//           if (installingWorker.state === 'installed') {
+//             if (navigator.serviceWorker.controller) {
+//               // At this point, the updated precached content has been fetched,
+//               // but the previous service worker will still serve the older
+//               // content until all client tabs are closed.
+//               console.log(
+//                 'New content is available and will be used when all ' +
+//                   'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+//               );
 
-              // Execute callback
-            } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
+//               // Execute callback
+//             } else {
+//               // At this point, everything has been precached.
+//               // It's the perfect time to display a
+//               // "Content is cached for offline use." message.
+//               console.log('Content is cached for offline use.');
 
-              // Execute callback
-            }
-          }
-        };
-      };
-    })
-    .catch(error => {
-      console.error('Error during service worker registration:', error);
-    });
-}
+//               // Execute callback
+//             }
+//           }
+//         };
+//       };
+//     })
+//     .catch(error => {
+//       console.error('Error during service worker registration:', error);
+//     });
+// }
